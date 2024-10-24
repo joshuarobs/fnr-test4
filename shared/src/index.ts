ad44e5787b7f7c1954dc7593065b9eb3e3fc39f3
Joshua Robillos <joshuarobs@icloud.com>
2024-10-24 13:17:28 +1100

    Added scroll area

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -10,3 +10,4 @@ export * from './components/ui/dropdown-menu';
 export * from './components/ui/select';
 export * from './components/ui/popover';
 export * from './components/ui/label';
+export * from './components/ui/scroll-area';
