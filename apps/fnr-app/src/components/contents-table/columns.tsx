0d5f9d572b8369e2e577f55134e7cd8f78a1a864
joshuarobs <52946881+joshuarobs@users.noreply.github.com>
2024-10-29 21:54:30 +1100

    fix columns.tsx errors and added const

--- apps/fnr-app/src/components/contents-table/columns.tsx
+++ apps/fnr-app/src/components/contents-table/columns.tsx
@@ -79,159 +79,166 @@ const SortableHeader = ({ column, title }: { column: any; title: string }) => (
 
 export const createColumns = (
   updateItem: (item: Item) => void
-): ColumnDef<Item>[] => [
-  {
-    accessorKey: ITEM_KEYS.ID,
-    header: ({ column }) => <SortableHeader column={column} title="ID" />,
-    cell: ({ row }) => {
-      const value = row.getValue(ITEM_KEYS.ID) as string;
-      return <IdCell value={value} />;
+): ColumnDef<Item>[] =>
+  [
+    {
+      accessorKey: ITEM_KEYS.ID,
+      header: ({ column }) => <SortableHeader column={column} title="ID" />,
+      cell: ({ row }) => {
+        const value = row.getValue(ITEM_KEYS.ID) as number;
+        return <IdCell value={value} />;
+      },
     },
-  },
-  {
-    accessorKey: ITEM_KEYS.GROUP,
-    header: ({ column }) => <SortableHeader column={column} title="Group" />,
-    cell: ({ row }) => {
-      const group = row.getValue(ITEM_KEYS.GROUP) as string;
-      return (
-        <div className={CELL_CONTENT_MARGIN}>
-          <GroupCell group={group} />
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: ITEM_KEYS.NAME,
-    header: ({ column }) => <SortableHeader column={column} title="Name" />,
-    cell: ({ row }) => {
-      return (
-        <div className={CELL_CONTENT_MARGIN}>
-          <ItemNameCell item={row.original} updateItem={updateItem} />
-        </div>
-      );
-    },
-    meta: {
-      headerClassName: 'min-w-[240px]',
-    },
-  },
-  {
-    accessorKey: ITEM_KEYS.STATUS,
-    header: ({ column }) => <SortableHeader column={column} title="Status" />,
-    meta: {
-      headerClassName: 'min-w-[96px]', // min-width, taking
-    },
-    cell: ({ row }) => {
-      const status = row.getValue(ITEM_KEYS.STATUS) as Item['status'];
-      return (
-        <div className={CELL_CONTENT_MARGIN}>
-          <ItemStatusBadge status={status} />
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: ITEM_KEYS.CATEGORY,
-    header: ({ column }) => <SortableHeader column={column} title="Category" />,
-    cell: ({ row }) => {
-      return (
-        <div className={CELL_CONTENT_MARGIN}>
-          {row.getValue(ITEM_KEYS.CATEGORY)}
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: ITEM_KEYS.MODEL_SERIAL_NUMBER,
-    header: 'Model/Serial number',
-    cell: ({ row }) => {
-      const modelSerialNumber = row.getValue(ITEM_KEYS.MODEL_SERIAL_NUMBER);
-      return modelSerialNumber ? (
-        <ModelSerialCell modelSerialNumber={modelSerialNumber as string} />
-      ) : null;
-    },
-  },
-  {
-    accessorKey: ITEM_KEYS.OIS_QUOTE,
-    header: () => <RightAlignedHeader>Insured's quote ($)</RightAlignedHeader>,
-    cell: ({ row }) => {
-      const oisQuote = row.getValue(ITEM_KEYS.OIS_QUOTE) as number;
-      const receiptPhotoUrl = row.original[ITEM_KEYS.RECEIPT_PHOTO_URL];
-      return (
-        <InsuredsQuoteCell
-          oisQuote={oisQuote}
-          receiptPhotoUrl={receiptPhotoUrl}
-        />
-      );
-    },
-  },
-  {
-    accessorKey: 'difference',
-    header: () => <RightAlignedHeader>Difference ($)</RightAlignedHeader>,
-    cell: ({ row }) => {
-      const item = row.original;
-      const oisQuote = item[ITEM_KEYS.OIS_QUOTE] as number | null;
-      const ourQuote = item[ITEM_KEYS.OUR_QUOTE] as number | null;
-
-      if (oisQuote === null || ourQuote === null) {
-        return <div className="text-right">N/A</div>;
-      }
-
-      if (oisQuote === ourQuote) {
+    {
+      accessorKey: ITEM_KEYS.GROUP,
+      header: ({ column }) => <SortableHeader column={column} title="Group" />,
+      cell: ({ row }) => {
+        const group = row.getValue(ITEM_KEYS.GROUP) as string;
         return (
-          <div className="text-right">
-            Same
-            <GreenTickIcon />
+          <div className={CELL_CONTENT_MARGIN}>
+            <GroupCell group={group} />
           </div>
         );
-      }
+      },
+    },
+    {
+      accessorKey: ITEM_KEYS.NAME,
+      header: ({ column }) => <SortableHeader column={column} title="Name" />,
+      cell: ({ row }) => {
+        return (
+          <div className={CELL_CONTENT_MARGIN}>
+            <ItemNameCell item={row.original} updateItem={updateItem} />
+          </div>
+        );
+      },
+      meta: {
+        headerClassName: 'min-w-[240px]',
+      },
+    },
+    {
+      accessorKey: ITEM_KEYS.STATUS,
+      header: ({ column }) => <SortableHeader column={column} title="Status" />,
+      meta: {
+        headerClassName: 'min-w-[96px]', // min-width, taking
+      },
+      cell: ({ row }) => {
+        const status = row.getValue(ITEM_KEYS.STATUS) as Item['status'];
+        return (
+          <div className={CELL_CONTENT_MARGIN}>
+            <ItemStatusBadge status={status} />
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: ITEM_KEYS.CATEGORY,
+      header: ({ column }) => (
+        <SortableHeader column={column} title="Category" />
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className={CELL_CONTENT_MARGIN}>
+            {row.getValue(ITEM_KEYS.CATEGORY)}
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: ITEM_KEYS.MODEL_SERIAL_NUMBER,
+      header: 'Model/Serial number',
+      cell: ({ row }) => {
+        const modelSerialNumber = row.getValue(ITEM_KEYS.MODEL_SERIAL_NUMBER);
+        return modelSerialNumber ? (
+          <ModelSerialCell modelSerialNumber={modelSerialNumber as string} />
+        ) : null;
+      },
+    },
+    {
+      accessorKey: ITEM_KEYS.OIS_QUOTE,
+      header: () => (
+        <RightAlignedHeader>Insured's quote ($)</RightAlignedHeader>
+      ),
+      cell: ({ row }) => {
+        const oisQuote = row.getValue(ITEM_KEYS.OIS_QUOTE) as number;
+        const receiptPhotoUrl = row.original[ITEM_KEYS.RECEIPT_PHOTO_URL];
+        return (
+          <InsuredsQuoteCell
+            oisQuote={oisQuote}
+            receiptPhotoUrl={receiptPhotoUrl}
+          />
+        );
+      },
+    },
+    {
+      accessorKey: 'difference',
+      header: () => <RightAlignedHeader>Difference ($)</RightAlignedHeader>,
+      cell: ({ row }) => {
+        const item = row.original;
+        const oisQuote = item[ITEM_KEYS.OIS_QUOTE] as number | null;
+        const ourQuote = item[ITEM_KEYS.OUR_QUOTE] as number | null;
 
-      const difference = calculateDifference(item);
-      return (
-        <div className="flex items-center justify-end">
-          <QuoteDifferenceIcon oisquote={oisQuote} ourquote={ourQuote} />
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: ITEM_KEYS.OUR_QUOTE,
-    meta: {
-      headerClassName: 'min-w-[112px]', // min-width, taking
-    },
-    header: () => <RightAlignedHeader>Our quote ($)</RightAlignedHeader>,
-    cell: ({ row }) => {
-      return <OurQuoteCell item={row.original} updateItem={updateItem} />;
-    },
-  },
-  {
-    accessorKey: ITEM_KEYS.ACTIONS,
-    header: () => <div />,
-    cell: ({ row }) => {
-      const payment = row.original;
+        if (oisQuote === null || ourQuote === null) {
+          return <div className="text-right">N/A</div>;
+        }
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() =>
-                navigator.clipboard.writeText(payment[ITEM_KEYS.ID].toString())
-              }
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+        if (oisQuote === ourQuote) {
+          return (
+            <div className="text-right">
+              Same
+              <GreenTickIcon />
+            </div>
+          );
+        }
+
+        const difference = calculateDifference(item);
+        return (
+          <div className="flex items-center justify-end">
+            <QuoteDifferenceIcon oisquote={oisQuote} ourquote={ourQuote} />
+          </div>
+        );
+      },
     },
-  },
-];
+    {
+      accessorKey: ITEM_KEYS.OUR_QUOTE,
+      meta: {
+        headerClassName: 'min-w-[112px]', // min-width, taking
+      },
+      header: () => <RightAlignedHeader>Our quote ($)</RightAlignedHeader>,
+      cell: ({ row }) => {
+        return <OurQuoteCell item={row.original} updateItem={updateItem} />;
+      },
+    },
+    {
+      accessorKey: ITEM_KEYS.ACTIONS,
+      header: () => <div />,
+      cell: ({ row }) => {
+        const payment = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <DotsHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() =>
+                  navigator.clipboard.writeText(
+                    payment[ITEM_KEYS.ID].toString()
+                  )
+                }
+              >
+                Copy payment ID
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View customer</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ] as const;
