4ca91043de680675c9dfdcf47e3c61d3fe852803
joshuarobs <52946881+joshuarobs@users.noreply.github.com>
2024-11-01 17:47:15 +1100

    UX Tooltip for quotedifficon

--- apps/fnr-app/src/components/contents-table/QuoteDifferenceIcon.tsx
+++ apps/fnr-app/src/components/contents-table/QuoteDifferenceIcon.tsx
@@ -1,6 +1,12 @@
 import { GreenTickIcon } from './GreenTickIcon';
 import { CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
 import { TriangleUpIcon, TriangleDownIcon } from '@radix-ui/react-icons';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@react-monorepo/shared';
 
 interface QuoteDifferenceIconProps {
   oisquote: number;
@@ -23,26 +29,58 @@ export const QuoteDifferenceIcon = ({
     position: 'relative' as const,
   };
 
+  const tooltipText =
+    diff > 0
+      ? `The insured's quote is higher than our quote by $${Math.abs(
+          diff
+        ).toFixed(2)}`
+      : `The insured's quote is lower than our quote by $${Math.abs(
+          diff
+        ).toFixed(2)}`;
+
   if (diff === 0) {
     return <GreenTickIcon />;
   } else if (diff > 0) {
     return (
-      <div className="flex items-center">
-        <CaretUpOutlined className={`${redColor} mr-1`} style={iconStyle} />
-        <span className={`${redColor} font-semibold`}>${diff.toFixed(2)}</span>
-      </div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="flex items-center">
+              <CaretUpOutlined
+                className={`${redColor} mr-1`}
+                style={iconStyle}
+              />
+              <span className={`${redColor} font-semibold`}>
+                ${diff.toFixed(2)}
+              </span>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>{tooltipText}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     );
   } else {
     return (
-      <div className="flex items-center">
-        <CaretDownOutlined
-          className={`${greenColor} mr-1`}
-          style={{ ...iconStyle, top: '2px' }}
-        />
-        <span className={`${greenColor} font-semibold`}>
-          ${Math.abs(diff).toFixed(2)}
-        </span>
-      </div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="flex items-center">
+              <CaretDownOutlined
+                className={`${greenColor} mr-1`}
+                style={{ ...iconStyle, top: '2px' }}
+              />
+              <span className={`${greenColor} font-semibold`}>
+                ${Math.abs(diff).toFixed(2)}
+              </span>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>{tooltipText}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     );
   }
 };
