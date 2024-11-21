342186c2dbcd000a8baea5a0802b0eac2efd47b9
joshuarobs <52946881+joshuarobs@users.noreply.github.com>
2024-11-21 18:38:55 +1100

    Remove item posts api as its redundant

--- apps/fnr-server/src/routes/items/index.ts
+++ apps/fnr-server/src/routes/items/index.ts
@@ -1,10 +1,8 @@
 import express, { Router } from 'express';
-import postRoutes from './post';
 import deleteRoutes from './delete';
 
 const router: Router = express.Router();
 
-router.use(postRoutes);
 router.use(deleteRoutes);
 
 export default router;
