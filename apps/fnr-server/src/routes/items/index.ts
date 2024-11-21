86619ea80d4d784573450e8bae1eaac6131732b7
Joshua Robillos <joshuarobs@icloud.com>
2024-11-21 15:38:27 +1100

    Split items apis into their own fiels

---
+++ apps/fnr-server/src/routes/items/index.ts
@@ -0,0 +1,14 @@
+import express, { Router } from 'express';
+import getRoutes from './get';
+import postRoutes from './post';
+import patchRoutes from './patch';
+import deleteRoutes from './delete';
+
+const router: Router = express.Router();
+
+router.use(getRoutes);
+router.use(postRoutes);
+router.use(patchRoutes);
+router.use(deleteRoutes);
+
+export default router;
